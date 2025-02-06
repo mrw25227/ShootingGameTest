a53afc4f75c0590b534667d20976f5016bb54064
HAO WANG <Andy_Wang>
2025-02-06 17:50:17 +0800

    update player move control

--- ShootingGame/Assets/Scripts/GameObjectControl/Controller.cs
+++ ShootingGame/Assets/Scripts/GameObjectControl/Controller.cs
@@ -1,16 +1,14 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
-public class Controller : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class Controller : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
     private Vector2 moveInput;
-    private Vector2 mouseMoveInput;
     private bool controllerIsDrag = false;
     private bool shootingBtnIsDown = false;
     private float controllerBoundRadius = 1.2f;
     private int controlFingerId = -1;
+    private Vector3 screenMoveInput = Vector3.zero;
 
     [SerializeField]
     Transform controlBtn;
@@ -25,45 +23,12 @@ public class Controller : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0) && controllerIsDrag) //«ö¤U·Æ¹«¥ªÁä
+        if (controllerIsDrag && screenMoveInput != Vector3.zero) //©ì¦²µe­±¾Þ±±¤è¦V
         {
-            int i = 0;
-            bool findFinger = false;
-            Touch touch;
-            for (; i < Input.touchCount; i++)
-            {
-                touch = Input.GetTouch(i);
-                Debug.LogWarning("finger index : " + i + " id: " + touch.fingerId);
-                if (touch.fingerId == controlFingerId)
-                {                    
-                    findFinger = true;
-                    break;
-                }
-            }
-            if (!findFinger)
-            {
-                Debug.LogWarning("controlFingerId is fail");
-            }
-            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            float dist = Vector2.Distance(pos, BtnInitPos);
-            if (dist > controllerBoundRadius)
-            {
-                mouseMoveInput.x = BtnInitPos.x + (pos.x - BtnInitPos.x) * (controllerBoundRadius / dist);
-                mouseMoveInput.y = BtnInitPos.y + (pos.y - BtnInitPos.y) * (controllerBoundRadius / dist);
-                controlBtn.position = mouseMoveInput;
-            }
-            else
-            {
-                mouseMoveInput.x = pos.x;
-                mouseMoveInput.y = pos.y;
-                controlBtn.position = mouseMoveInput;
-            }
-            moveInput = (mouseMoveInput - BtnInitPos) / controllerBoundRadius;
-            //Debug.Log(moveInput);
-            EventManager.Instance.OnControllerClickInvoke(moveInput);
+            DragMoveControl(screenMoveInput);
 
         } 
-        else
+        else //Áä½L¾Þ±±¤è¦V
         {
             moveInput.x = Input.GetAxisRaw("Horizontal");
             moveInput.y = Input.GetAxisRaw("Vertical");
@@ -85,13 +50,13 @@ public class Controller : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.LogWarning("OnContollerBtnDown eventData.pointerId: " + eventData.pointerId);
-        controlFingerId = eventData.pointerId;
-#if UNITY_ANDROID && !UNITY_EDITOR
-            controllerIsDrag = true;
-#else
+#if !UNITY_ANDROID || UNITY_EDITOR
         if (eventData.button == PointerEventData.InputButton.Left)
         {
+#endif
             controllerIsDrag = true;
+            screenMoveInput = Camera.main.ScreenToWorldPoint(eventData.position);
+#if !UNITY_ANDROID || UNITY_EDITOR
         }
 #endif
     }
@@ -99,16 +64,43 @@ public class Controller : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     public void OnPointerUp(PointerEventData eventData)
     {
         Debug.LogWarning("OnContollerBtnUp eventData.pointerId: " + eventData.pointerId);
-#if UNITY_ANDROID && !UNITY_EDITOR
-        controllerIsDrag = false;
-        controlBtn.position = BtnInitPos;
-#else
+#if !UNITY_ANDROID || UNITY_EDITOR
         if (eventData.button == PointerEventData.InputButton.Left)
         {
-            controllerIsDrag = false;
-        }
-        controlBtn.position = BtnInitPos;
 #endif
+            controllerIsDrag = false;
+            controlBtn.position = BtnInitPos;
+            screenMoveInput = Vector3.zero;
+
+#if !UNITY_ANDROID || UNITY_EDITOR
+        }
+#endif
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        screenMoveInput = Camera.main.ScreenToWorldPoint(eventData.position);
+
+        Debug.LogWarning("OnDrag eventData.pointerId: " + eventData.pointerId + "eventData.position: " + screenMoveInput);
+        
+    }
+
+    private void DragMoveControl(Vector3 pos)
+    {
+        float dist = Vector2.Distance(pos, BtnInitPos);
+        Vector2 InputTemp = Vector2.zero;
+        if (dist > controllerBoundRadius)
+        {
+            InputTemp.x = BtnInitPos.x + (pos.x - BtnInitPos.x) * (controllerBoundRadius / dist);
+            InputTemp.y = BtnInitPos.y + (pos.y - BtnInitPos.y) * (controllerBoundRadius / dist);            
+        }
+        else
+        {
+            InputTemp.x = pos.x;
+            InputTemp.y = pos.y;
+        }
+        controlBtn.position = InputTemp;
+        moveInput = (InputTemp - BtnInitPos) / controllerBoundRadius;
+        EventManager.Instance.OnControllerClickInvoke(moveInput);
     }
 
     public void OnShootingBtnDown(BaseEventData eventData)
@@ -119,4 +111,6 @@ public class Controller : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         shootingBtnIsDown =false;
     }
+
+    
 }
